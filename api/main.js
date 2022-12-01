5730e16735ba702d6682be77ba52a67cc1e169fb
Adrian Perez <adrian.perez.conde22@gmail.com>
2022-12-01 18:20:47 +0100

    feat: ceetiz scraper done

--- api/main.js
+++ api/main.js
@@ -7,6 +7,7 @@ const tiqetsScraper = require("./scraper-websites/tiqets");
 const eventbriteScraper = require("./scraper-websites/eventbrite");
 const ticketswapScraper = require("./scraper-websites/ticketswap");
 const musementScraper = require("./scraper-websites/musement");
+const ceetizScraper = require("./scraper-websites/ceetiz");
 
 async function data() {
   const bookingData = await bookingScraper.booking();
@@ -16,6 +17,7 @@ async function data() {
   const eventbriteData = await eventbriteScraper.eventbrite();
   const ticketswapData = await ticketswapScraper.ticketswap();
   const musementData = await musementScraper.musement();
+  const ceetizData = await ceetizScraper.ceetiz();
 
   const eventsData = {
     ...bookingData,
@@ -25,6 +27,7 @@ async function data() {
     ...eventbriteData,
     ...ticketswapData,
     ...musementData,
+    ...ceetizData,
   };
 
   const eventsContent = JSON.stringify(eventsData);
---
+++ api/scraper-websites/ceetiz.js
@@ -0,0 +1,133 @@
+const puppeteer = require("puppeteer-core");
+
+async function ceetiz() {
+  const browser = await puppeteer.launch({
+    executablePath:
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    headless: true,
+    args: ["--incognito"],
+  });
+
+  const context = await browser.createIncognitoBrowserContext();
+
+  const page = await context.newPage();
+
+  await page.setExtraHTTPHeaders({
+    "Accept-Language": "en",
+  });
+
+  // Going to Booking website:
+
+  let cityName = "amsterdam";
+
+  await page.goto(`https://www.ceetiz.com/${cityName}`);
+
+  function delay(time) {
+    return new Promise(function (resolve) {
+      setTimeout(resolve, time);
+    });
+  }
+
+  await delay(2000);
+
+  // Taking URL of each event:
+
+  await page.waitForSelector(".home--dest__intro__title");
+
+  const events = await page.evaluate(() => {
+    const attractions = document.querySelectorAll(
+      "#featured-list > ul > li > article > a"
+    );
+
+    const links = [];
+    for (let attraction of attractions) {
+      links.push(attraction.href);
+    }
+    return links;
+  });
+
+  await delay(2000);
+
+  // Array created and loop on every event to take the data:
+
+  const thingsToDo = [];
+  for (let event of events) {
+    await page.goto(event);
+    await page.waitForSelector(
+      "div.flex-grow.flex.flex-col.justify-between.gap-y-4 > div > h1"
+    );
+
+    await delay(1500);
+
+    const attractionData = await page.evaluate(() => {
+      const tmp = {};
+      tmp.title = document
+        .querySelector(
+          "div.flex-grow.flex.flex-col.justify-between.gap-y-4 > div > h1"
+        )
+        .textContent.trim();
+      try {
+        tmp.price = document
+          .querySelector(
+            "#reservation-wrapper > div > div.flex.flex-col.items-center.mt-4 > div > div.font-extrabold.text-xl.md\\:text-3xl.text-primary"
+          )
+          .textContent.trim();
+      } catch (error) {
+        tmp.price = "N.A.";
+      }
+      try {
+        tmp.duration = document
+          .querySelector(
+            "#informations > section > div.flex.flex-row.flex-wrap.items-center.sm\\:pr-5.sm\\:odd\\:border-l.odd\\:border-grey-300.mb-7 > h3.flex.w-full.sm\\:w-1\\/2.p-2.items-start.sm\\:pr-5.mt-2 > div.ml-auto.font-bold.text-right > span"
+          )
+          .textContent.trim();
+      } catch (error) {
+        tmp.duration = "N.A.";
+      }
+      try {
+        tmp.time = document
+          .querySelector(
+            "#informations > section > div.flex.flex-row.flex-wrap.items-center.sm\\:pr-5.sm\\:odd\\:border-l.odd\\:border-grey-300.mb-7 > h3.flex.w-full.sm\\:w-1\\/2.p-2.items-start.pr-5.mt-3 > div.ml-auto.font-bold.text-right"
+          )
+          .textContent.trim();
+      } catch (error) {
+        tmp.time = "N.A.";
+      }
+      try {
+        tmp.rating = document
+          .querySelector(
+            "#reviews > div > section > div > div.flex.flex-col.items-center.md\\:flex-row.md\\:items-start > div.mr-2 > div"
+          )
+          .textContent.trim();
+      } catch (error) {
+        tmp.rating = "N.A.";
+      }
+      try {
+        tmp.description = document
+          .querySelector("#informations > div:nth-child(2) > div:nth-child(2)")
+          .textContent.trim();
+      } catch (error) {
+        tmp.description = "N.A.";
+      }
+
+      tmp.url = window.location.href;
+
+      return tmp;
+    });
+
+    thingsToDo.push(attractionData);
+  }
+
+  const ceetizData = thingsToDo.reduce((acc, item) => {
+    acc[item.title] = item;
+    return acc;
+  }, {});
+
+  await context.close();
+
+  return ceetizData;
+}
+
+module.exports = {
+  ceetiz: ceetiz,
+};
